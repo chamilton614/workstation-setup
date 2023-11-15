95e87b4d7d74b7c8b84d4fecc321c19349c4ad4f
Chad J. Hamilton <chad.j.hamilton@gmail.com>
2023-11-15 16:26:17 -0500

    remove local gpg file

--- install-vscode.sh
+++ install-vscode.sh
@@ -10,6 +10,7 @@ sudo apt install software-properties-common apt-transport-https wget gpg
 #Download and Import Microsoft's GPG Key
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+rm -f packages.microsoft.gpg
 
 #Add the Microsoft Visual Studio Code Repository
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
