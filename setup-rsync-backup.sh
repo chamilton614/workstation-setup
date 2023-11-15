db53a934f454641f2a1911267805ad955340950e
Chad J. Hamilton <chad.j.hamilton@gmail.com>
2023-11-15 18:09:42 -0500

    initial revision

---
+++ setup-rsync-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Description:
+#This script is used to setup an rsync backup on a Workstation
+#Reference: https://www.howtogeek.com/135533/how-to-use-rsync-to-backup-your-data-on-linux/
+    https://ostechnix.com/backup-entire-linux-system-using-rsync/
+#Tested on Ubuntu 22.04
+
+#Install Prerequisites - Ubuntu
+sudo apt update
+
+#Install rsync
+sudo apt install rsync -y
+
+#Source Directory to Backup
+SOURCE_DIR=/data2/home/chamilton/
+
+#Destination Directory to Save Backup
+DESTINATION_DIR=/data/backups/data2-home-chamilton/
+
+#Rsync Options
+RSYNC_OPTIONS_1=-ravP
+RSYNC_OPTIONS_2=--delete
+
+#Rsync Command
+rsync ${RSYNC_OPTIONS_1} ${RSYNC_OPTIONS_2} ${SOURCE_DIR} ${DESTINATION_DIR}
+
+
+
+
