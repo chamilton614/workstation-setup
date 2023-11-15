0112d3795c00a5627a71c4350cea8370a87f1fbb
Chad J. Hamilton <chad.j.hamilton@gmail.com>
2023-11-15 16:23:33 -0500

    initial revision

---
+++ install-vscode.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Description:
+#This script is used to install Visual Studio Code on a Workstation
+#Tested on Ubuntu 22.04
+
+#Install Prerequisites - Ubuntu
+sudo apt install software-properties-common apt-transport-https wget gpg
+
+#Download and Import Microsoft's GPG Key
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+
+#Add the Microsoft Visual Studio Code Repository
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+#Run System Update to update the APT repository index
+sudo apt update
+
+#Install Visual Studio Code
+sudo apt install code
+
+
+
