33f703fe7a031266708dc4cfea30dfeea2a5f366
Chad J. Hamilton <chad.j.hamilton@gmail.com>
2023-11-26 10:30:01 -0500

    update to put scripts in setup directory

--- docker/msedge/run.sh
+++ docker/msedge/run.sh
@@ -30,15 +30,50 @@ creationDate=$(date '+%Y%m%d-%H%M%S')
 #Create Edge Profile for Container
 cp -R ${HOME}/.config/microsoft-edge-container ${HOME}/.config/microsoft-edge-container-$creationDate
 
-docker run -it \
+#docker run -it \
+#    --rm \
+#    --net host \
+#    --cpuset-cpus 2 \
+#    --memory 1024mb \
+#    -v /etc/localtime:/etc/localtime:ro \
+#    -v /tmp/.X11-unix:/tmp/.X11-unix \
+#    -e "DISPLAY=unix$DISPLAY" \
+#    --security-opt seccomp=unconfined \
+#    -v "${HOME}/Downloads:/home/edge/Downloads" \
+#    -v "${HOME}/Pictures:/home/edge/Pictures" \
+#    -v "${HOME}/.config/microsoft-edge-container-$creationDate:/data" \
+#    -v /var/run/dbus:/var/run/dbus \
+#    -v /dev/shm:/dev/shm \
+#    -v /etc/hosts:/etc/hosts \
+#    -v "/etc/alsa:/etc/alsa" \
+#    -v "/usr/share/alsa:/usr/share/alsa" \
+#    -v "${HOME}/.config/pulse:/.config/pulse" \
+#    -v "/run/user/$UID/pulse/native:/run/user/$UID/pulse/native" \
+#    --env "PULSE_SERVER=unix:/run/user/$UID/pulse/native" \
+#    --device /dev/snd \
+#    --device /dev/dri \
+#    --device /dev/video0 \
+#    --device /dev/usb \
+#    --device /dev/bus/usb \
+#    --group-add audio \
+#    --group-add video \
+#    --env ALSA_CARD="HDA Intel" \
+#    --name msedge-$creationDate \
+#    msedge-opengl --user-data-dir=/data "$*"
+
+#--security-opt seccomp=${HOME}/msedge.json \
+#--security-opt seccomp=unconfined \
+
+#cp /usr/share/containers/seccomp.json $HOME
+
+podman run -it \
     --rm \
     --net host \
-    --cpuset-cpus 2 \
     --memory 1024mb \
     -v /etc/localtime:/etc/localtime:ro \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e "DISPLAY=unix$DISPLAY" \
-    --security-opt seccomp=unconfined \
+    --security-opt seccomp=${HOME}/seccomp.json \
     -v "${HOME}/Downloads:/home/edge/Downloads" \
     -v "${HOME}/Pictures:/home/edge/Pictures" \
     -v "${HOME}/.config/microsoft-edge-container-$creationDate:/data" \
@@ -59,10 +94,16 @@ docker run -it \
     --group-add video \
     --env ALSA_CARD="HDA Intel" \
     --name msedge-$creationDate \
-    msedge-opengl --user-data-dir=/data "$*"
+    msedge-pm --user-data-dir=/data "$*"
 
-#--security-opt seccomp=${HOME}/msedge.json \
-#--security-opt seccomp=unconfined \
+
+
+
+
+
+
+
+
 
 # docker run -it \
 #	--net host \ # may as well YOLO
